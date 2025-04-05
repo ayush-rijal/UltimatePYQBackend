e591dbd605a11275aecd1492d7ed47a77868be01
Quester <quester561@gmail.com>
2025-04-05 22:51:51 +0545

    dynamic category self refrential

--- quizAPI/admin.py
+++ quizAPI/admin.py
@@ -1,21 +1,17 @@
 from django.contrib import admin
-from .models import Category0, Category1, Questions_file, Question, Choice, SubjectCategory,UserResponse,UserQuizResult,Leaderboard
+from .models import Category, Questions_file, Question, Choice, SubjectCategory,UserResponse,UserQuizResult,Leaderboard
 
-
-class Category0Admin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-
-class Category1Admin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('name', 'parent','level')
+    search_fields=('name',)   
+   
 
 class Questions_fileAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category0', 'category1','created_at')    
-    search_fields = ('title', 'category0__name')
-    list_filter = ('category0', 'created_at')
+    list_display = ('title', 'category','created_at')    
+    search_fields = ('title', 'category__name')
+    list_filter = ( 'created_at','category')
     fieldsets = (
-        (None, {'fields': ('title', 'description', 'category0', 'category1','questions_file')}),
+        (None, {'fields': ('title', 'description', 'category','questions_file')}),
         ('Dates', {'fields': ('created_at', 'updated_at'), 'classes': ('collapse',)}),
     )
     readonly_fields = ('created_at', 'updated_at')
@@ -52,8 +48,7 @@ class LeaderboardAdmin(admin.ModelAdmin):
 
 
 
-admin.site.register(Category0,Category0Admin)
-admin.site.register(Category1,Category1Admin)
+admin.site.register(Category,CategoryAdmin)
 admin.site.register(Questions_file, Questions_fileAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice, ChoiceAdmin)
