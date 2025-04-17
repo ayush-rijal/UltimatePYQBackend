c5db2916117e578b2a14f573056b8fd0503b57db
Quester <quester561@gmail.com>
2025-04-17 12:40:13 +0545

    dynamic category and image

--- pyqBackend/urls.py
+++ pyqBackend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,5 @@ urlpatterns = [
 path('userapi/',include('users.urls')),
 path('todoapi/', include('todoList.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
